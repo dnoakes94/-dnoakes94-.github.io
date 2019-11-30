1dba3cf5b80f0d85edc8ab1c465c9632c177326b
Dan Noakes <dnoakes@flemingcollege.ca>
2019-11-30 14:15:15 -0500

    Some edits, still testing

--- diffLeveling.py
+++ diffLeveling.py
@@ -1,2 +1,54 @@
 #Differential Leveling Computation
-# I am testing some changes
+#Caroline Bull, Christine Lacanilao, Daniel Noakes
+
+#Import math and csv module
+import math
+import csv
+
+#write helper files first
+#anything that will read or write
+
+def instHeight(insteight, elevation, bckSight)
+        instrumentHeight = 
+
+try:
+    while true:
+        print "Differential Leveling Computation"
+
+        manualOrCSV = raw_input("Type of computation: manual or csv ") 
+        
+        # if manualOrCSV = "manual"
+
+        #     if
+        #      if
+        #
+    
+
+        else:
+
+
+
+
+
+
+
+
+
+except Exception, message:
+    print "An error occured. Please try again."
+    print message
+
+except NameError:
+    print "Variable is not found in local space. Please try again."
+
+except IndexError:
+    print "Index of a sequence is out of range. Please try again."
+
+except TypeError:
+    print "You cant add strings and integers. Please try again."
+
+except ValueError:
+    print "Value must be a number. Please try again."
+
+except ZeroDivisionError:
+    print "You can't divide by zero. Please try again."
